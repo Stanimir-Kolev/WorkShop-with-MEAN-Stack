7ebc893dc9ee825e1b4d57df21aa8c3db41d3891
Stanimir-Kolev <stmklv@gmail.com>
2017-11-27 11:50:50 +0200

    implemented login at the server

--- server/config/express.js
+++ server/config/express.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var stylus = require('stylus');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 
 //config for express
 module.exports = function(app, config) {
@@ -9,6 +12,14 @@ module.exports = function(app, config) {
     // use body-parser for express
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+    // use session
+    app.set('trust proxy', 1); // trust first proxy
+    app.use(session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true }
+    }));
     // setup for stylus
     app.use(stylus.middleware({
         src: config.rootPath + '/public',
@@ -16,5 +27,8 @@ module.exports = function(app, config) {
             return stylus(str).set('filename', path);
         }
     }));
+    // set (use) config
+    app.use(passport.initialize());
+    app.use(passport.session());
     app.use(express.static(config.rootPath + '/public'));
 }
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalPassport = require('passport-local');
 
 module.exports = function(config) {
     // setup for mongoDB
@@ -21,4 +23,63 @@ module.exports = function(config) {
         // for error (node:4736)
     mongoose.Promise = global.Promise;
 
-}
+    var userSchema = mongoose.Schema({
+        username: String,
+        firstName: String,
+        lastName: String
+            // salt: String,
+            // hashPass: String
+    });
+
+    var User = mongoose.model('User', userSchema);
+
+    User.find({}).exec(function(err, collection) {
+        if (err) {
+            console.log('Cannot find user: ' + err);
+            return;
+        }
+        if (collection.length === 0) {
+            User.create({
+                username: 'stanimir.kolev',
+                firstName: 'Stanimir',
+                lastName: 'Kolev'
+                    // salt: String,
+                    // hashPass: String  
+            })
+        }
+    })
+
+    // use passport config from documentation
+    passport.use(new LocalPassport(function(username, password, done) {
+        User.findOne({ username: username }).exec(function(err, user) {
+            if (err) {
+                console.log('Error loading user: ' + err);
+                return;
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        })
+    }));
+    passport.serializeUser(function(user, done) {
+        if (user) {
+            return done(null, user._id);
+        }
+    })
+
+    passport.deserializeUser(function(id, done) {
+        User.findOne({ _id: id }).exec(function(err, user) {
+            if (err) {
+                console.log('Error loading user: ' + err);
+                return;
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        })
+    })
+};
